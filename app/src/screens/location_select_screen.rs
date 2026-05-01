18d158a68f0e6aa5cab72393df29c619bf8662dc
Ben Halstead <benhalstd@gmail.com>
2026-05-01 16:47:33 +1200

    fuzzy matching

--- app/src/screens/location_select_screen.rs
+++ app/src/screens/location_select_screen.rs
@@ -1,3 +1,4 @@
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Modifier, Style},
@@ -18,14 +19,20 @@ pub struct LocationSelectScreen {
     idx: usize,
     location_tags: Vec<LocationTag>,
     query: Option<String>,
+    matcher: SkimMatcherV2,
+    matches: Vec<usize>,
 }
 
 impl LocationSelectScreen {
     pub fn new(db: &FileDB) -> Self {
+        let tags = db.get_tags();
+        let n_tags = tags.len();
         Self {
             idx: 0,
-            location_tags: db.get_tags(),
+            location_tags: tags,
             query: None,
+            matcher: SkimMatcherV2::default(),
+            matches: (0..n_tags).collect(),
         }
     }
 
@@ -40,7 +47,13 @@ impl LocationSelectScreen {
     }
 
     fn select(&mut self, db: &FileDB) -> (Vec<Update>, Vec<Message>) {
-        let selected_tag = &self.location_tags[self.idx];
+        let items: Vec<&LocationTag> = self
+            .matches
+            .iter()
+            .map(|&i| &self.location_tags[i])
+            .collect();
+
+        let selected_tag = &items[self.idx];
         match db.get_by_id(&selected_tag.id) {
             Some(loc) => {
                 self.query = None;
@@ -68,14 +81,38 @@ impl Component for LocationSelectScreen {
         if let Some(query) = &mut self.query {
             // search mode — chars feed the query, arrows navigate, esc exits
             match msg {
-                Message::Char(c) => query.push(*c),
+                Message::Char(c) => {
+                    query.push(*c);
+                    if let Some(matches) =
+                        recompute_matches(&self.query, &self.location_tags, &self.matcher)
+                    {
+                        self.matches = matches;
+                    }
+                }
                 Message::Backspace => {
                     query.pop();
+                    if let Some(matches) =
+                        recompute_matches(&self.query, &self.location_tags, &self.matcher)
+                    {
+                        self.matches = matches;
+                    }
                 }
-                Message::Esc => self.query = None,
+                Message::Esc => {
+                    self.query = None;
+                    if let Some(matches) =
+                        recompute_matches(&self.query, &self.location_tags, &self.matcher)
+                    {
+                        self.matches = matches;
+                    }
+                }
                 Message::Up => self.move_up(),
                 Message::Down => self.move_down(),
-                Message::Enter => return self.select(db),
+                Message::Enter => {
+                    let selected = self.select(db);
+                    self.query = None;
+                    self.matches = (0..self.location_tags.len()).collect();
+                    return selected;
+                }
                 _ => {}
             }
         } else {
@@ -84,7 +121,12 @@ impl Component for LocationSelectScreen {
                 Message::Up | Message::Char('w') | Message::Char('k') => self.move_up(),
                 Message::Down | Message::Char('s') | Message::Char('j') => self.move_down(),
                 Message::Char('/') => self.query = Some(String::new()),
-                Message::Enter => return self.select(db),
+                Message::Enter => {
+                    let selected = self.select(db);
+                    self.query = None;
+                    self.matches = (0..self.location_tags.len()).collect();
+                    return selected;
+                }
                 _ => {}
             }
         }
@@ -98,9 +140,12 @@ impl Component for LocationSelectScreen {
             .split(area);
 
         let items: Vec<ListItem> = self
-            .location_tags
+            .matches
             .iter()
-            .map(|l| ListItem::new(format!("{} - {}", l.id, l.name)))
+            .map(|&i| {
+                let tag = &self.location_tags[i];
+                ListItem::new(format!("{} - {}", tag.id, tag.name))
+            })
             .collect();
         let list = List::new(items)
             .block(Block::default().borders(Borders::all()).title("Locations"))
@@ -116,4 +161,30 @@ impl Component for LocationSelectScreen {
         };
         frame.render_widget(p, layout[1]);
     }
+}
+
+fn recompute_matches(
+    query: &Option<String>,
+    tags: &[LocationTag],
+    matcher: &SkimMatcherV2,
+) -> Option<Vec<usize>> {
+    if let Some(query_str) = query {
+        let mut hits: Vec<(usize, i64)> = tags
+            .iter()
+            .enumerate()
+            .filter_map(|(i, t)| {
+                matcher
+                    .fuzzy_match(tag_to_search_str(&t), &query_str)
+                    .map(|score| (i, score))
+            })
+            .collect();
+        hits.sort_by(|a, b| b.1.cmp(&a.1)); // best first
+        let matched_indicies = hits.into_iter().map(|(i, _)| i).collect();
+        return Some(matched_indicies);
+    }
+    None
+}
+
+fn tag_to_search_str(tag: &LocationTag) -> &str {
+    &tag.name
 }
