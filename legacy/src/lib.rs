52cfc27d6aeb2539d7ad9898591022d5141e7ad3
Ben Halstead <benhalstd@gmail.com>
2026-05-03 11:39:20 +1200

    clean

--- legacy/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod db;
-pub mod event_handling;
-pub mod model;
-pub mod tui;
-pub mod view;
--- legacy/src/model/location.rs
+++
@@ -1,9 +0,0 @@
-use geo::Point;
-use geo::Polygon;
-
-pub struct Location {
-    id: String,
-    name: String,
-    world_centroid: Point,
-    polygons: Vec<Polygon>,
-}
